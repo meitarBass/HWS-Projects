ba53cbb3b22a60e91e88fc6cb14729b0f3b62b03
meitarbasson <meitarbass@gmail.com>
2024-09-18 11:48:25 +0300

    Project 7 - day 33

--- Project7/Project7/AppDelegate.swift
+++ Project7/Project7/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  Project7
-//
-//  Created by Meitar Basson on 18/09/2024.
-//
-
 import UIKit
 
 @main
--- Project7/Project7/SceneDelegate.swift
+++ Project7/Project7/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  Project7
-//
-//  Created by Meitar Basson on 18/09/2024.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
